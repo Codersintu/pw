c3165d163ad183b1f0c5bfe728f94a706416eb0b
Codersintu <skmahto9199@gmail.com>
2025-08-25 19:34:44 +0530

    complete navbar

--- src/component/navbar/Courses.jsx
+++ src/component/navbar/Courses.jsx
@@ -1,90 +1,10 @@
 import React, { useState } from 'react'
 import rightarr from "../../assets/rightarr.png"
+import { courses } from '../../data'
 
 function Courses() {
   const [activeIndex,setActiveIndex]=useState(null)
-  const courses = [
-   {
-    title: "Competitive Exams",
-    desc: "IIT JEE, NEET, ESE, GATE, AE/JE, Olympiad",
-    items: [ { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },],
-  },
-  {
-    title: "Only IAS",
-    desc: "UPSC, State PSC",
-    items: [ { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },], 
-  },
-  {
-    title: "School Preparation",
-    desc: "Foundation (Class 6-10), Commerce, Arts, CuriousJr, Science, International Boards",
-    items: [ { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },],
-  },
-  {
-    title: "Govt Exam",
-    desc: "SSC, Banking, Judiciary, Teaching, Railway, UP Exams, Defence, etc.",
-    items: [ { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },],
-  },
-  {
-    title: "UG & PG Entrance Exams",
-    desc: "MBA, IPMAT, IIT JAM, CSIR NET, LAW, CUET, UGC NET, GMAT, Design & Architecture, CUET PG, NEET PG, Pharma",
-    items: [
-      { name: "MBA", icon: "📚" },
-      { name: "IPMAT", icon: "🎓" },
-      { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },
-      { name: "Design & Architecture", icon: "📐" },
-      { name: "CUET PG", icon: "✅" },
-      { name: "NEET PG", icon: "🧬" },
-      { name: "Pharma", icon: "💊" },
-    ],
-  },
-  {
-    title: "Finance",
-    desc: "CA, CS, ACCA",
-    items: [ { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },],
-  },
-  {
-    title: "Others",
-    desc: "Online Degrees, Financial Certification, Private Banking",
-    items: [ { name: "IIT JAM", icon: "⚛️" },
-      { name: "CSIR NET", icon: "🔬" },
-      { name: "LAW", icon: "📜" },
-      { name: "CUET", icon: "🎓" },
-      { name: "UGC NET", icon: "🏛️" },
-      { name: "GMAT", icon: "📘" },],
-  },
- 
-];
-
+  
   return (
     <div onMouseLeave={() => setActiveIndex(null)}  className="absolute left-0 top-full hidden w-[1100px] bg-white shadow-lg border border-gray-200 group-hover:block">
          <div className="flex w-[1100px] ">
@@ -104,8 +24,8 @@ function Courses() {
             </div>
             {/* right side */}
              <div className="flex-4 p-6 grid grid-cols-2 gap-4 cursor-pointer">
-         {activeIndex !== null &&
-          courses[activeIndex].items.map((item, i) => (
+           {activeIndex !== null &&
+           courses[activeIndex].items.map((item, i) => (
             <div
               key={i}
               className="flex items-center gap-3 p-3 border rounded-lg shadow-2xl bg-white hover:shadow-md transition"
